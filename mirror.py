1d2a50208b9766681a249128703926a228caa2b3
Dannii Willis <curiousdannii@gmail.com>
2010-06-30 15:38:06 +1000

    Add support for big file requests, by making urlfetch requests with a Range header.

--- mirror.py
+++ mirror.py
@@ -11,7 +11,7 @@ from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 
 # Maximum size of a story file we'll download, in bytes.
-MAX_FILE_SIZE = 1024 * 1024
+MAX_FILE_SIZE = 1024 * 1000
 
 def get(url):
 	'''Get a file, using memcache if possible'''
@@ -23,16 +23,16 @@ def get(url):
 	
 	# Missed! Attempt to download it now
 	else:
-		result = urlfetch.fetch(url)
-		data = result.content
+		result = urlfetch.fetch(url, allow_truncated=True)
 		
 		# HTTP Status code
 		if result.status_code != 200:
 			raise Exception('got status code %d for url %s' % (result.status_code, url))
 		
-		# Check if it's too big (but won't urlfetch complain first?)
-		if len(result.content) > MAX_FILE_SIZE:
-			raise Exception('file too large')
+		# Check for oversized requests
+		data = result.content
+		if result.content_was_truncated:
+			data += fetch_big(url, int(result.headers['Content-Length']), len(data) - 1)
 		
 		# Check it's a valid glulx/zcode story file
 		if data.startswith('FORM') or data.startswith('Glul') or ord(data[0]) < 9:
@@ -40,8 +40,34 @@ def get(url):
 		else:
 			raise Exception('url does not contain a story file')
 		
-		# All good... cache it and return
-		if not memcache.add(url, data, 3600):
-			logging.error('Memcache set failed for url ' + url)
+		# All good... cache it and return (don't cache big files for now)
+		if len(data) <= MAX_FILE_SIZE:
+			if not memcache.add(url, data, 3600):
+				logging.error('Memcache set failed for url ' + url)
 		
 		return data
+
+def fetch_big(url, size, last):
+	'''Get the rest of a big file'''
+	# Consider changing later to request the parts async
+		
+	# Range for the next request
+	minrange = last + 1
+	maxrange = (last + MAX_FILE_SIZE)
+	# Can we trust the Content-Length header? What if it's gzipped?
+	if maxrange >= size:
+		maxrange = size - 1
+	headers = {'Range': 'bytes=%d-%d' % (minrange, maxrange)}
+	
+	# Make each URL unique, otherwise the cache will only return the first part (What about the headers?!?)
+	tempurl = url + '#' + str(minrange)
+	
+	# Request the next part of the file
+	request = urlfetch.fetch(tempurl, headers=headers, allow_truncated=True)
+	data = request.content
+	
+	# If that's not all, recursively fetch the next part
+	if maxrange < size - 1:
+		data += fetch_big(url, size, maxrange)
+	
+	return data
