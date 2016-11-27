50548c1928b09603a20c16522f49f9120d4211ce
Dannii Willis <curiousdannii@gmail.com>
2016-11-27 13:06:00 +1000

    Stop filtering for Zcode and Glulx files so that hugojs can use it Update the code for the current Python environment

--- main.py
+++ main.py
@@ -7,11 +7,11 @@
 
 import base64
 import hashlib
+import json
 import logging
 import sys
 import traceback
 
-from django.utils import simplejson
 from google.appengine.api import memcache
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -21,81 +21,81 @@ import mirror
 
 class ProxyHandler(webapp.RequestHandler):
 	'''The parchment-proxy server itself'''
-	
+
 	def initialize(self, request, response):
 		webapp.RequestHandler.initialize(self, request, response)
-		
+
 		# Add our code var
 		self.code = 200
-	
+
 	def get(self):
 		# Parameters
 		url = self.request.get('url').replace(' ', '%20')
 		callback = self.request.get('callback')
 		encode = self.request.get('encode')
 		ifnonematch = self.request.headers.get('If-None-Match') or ''
-		
+
 		if not url:
 			# Show the home page
 			self.redirect('/')
 			return
-		
+
 		# Set headers for allowing cross domain XHR and content type
 		self.response.headers['Access-Control-Allow-Origin'] = '*'
 		self.response.headers['Content-Type'] = 'text/plain; charset=ISO-8859-1'
-		
+
 		# Check for a cached hash
 		hash = memcache.get(url + '_hash')
 		if hash == ifnonematch:
 			self.response.set_status(304)
 			self.response.headers['ETag'] = hash
 			return
-		
+
 		# Get this URL
 		data = mirror.get(url)
-		
+
 		# Generate the hash if needed
 		if not hash:
 			hash = '"' + hashlib.md5(data).hexdigest() + '"'
 			if not memcache.add(url + '_hash', hash, 3600):
 				logging.error('Memcache set failed for hash of url ' + url)
-		
+
 		# Base64 encode the data if required
 		if encode == 'base64':
 			data = base64.b64encode(data)
-		
+
 		# Handle a callback function
 		if callback:
 			# Warning, data must be escaped too
 			data = callback + '("' + data + '")'
 			self.response.headers['Content-Type'] = 'text/javascript'
-		
+
 		# Sent the data
 		self.response.headers['ETag'] = hash
 		self.response.out.write(data)
-		
+
 	def options(self):
 		# Send Access-Control headers for preflighted requests
-		
+
 		self.response.headers['Access-Control-Allow-Origin'] = '*'
 		self.response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
 		self.response.headers['Access-Control-Allow-Headers'] = self.request.headers.get('Access-Control-Request-Headers') or ''
-	
+
 	def handle_exception(self, e, debug):
 		# Handle exceptions
 		self.response.clear()
-		
+
 		# Set the HTTP status code
 		if self.code == 200:
 			self.code = 500
 		self.response.set_status(self.code)
-		
+
 		# Send the exception message as a header
 		self.response.headers['x-exception'] = repr(e)
-		
+
 		# Write out the error message
 		self.response.out.write(e)
-		
+
 		# Log this error too
 		logging.error(repr(e))
 		logging.error(''.join(traceback.format_tb(sys.exc_info()[2])))
@@ -115,13 +115,13 @@ class LegacyHandler(webapp.RequestHandler):
 			url = url.replace(" ", "%20")
 			data = mirror.get(url)
 			response = {'data': base64.b64encode(data)}
-		  
+
 		except Exception, e:
 			response = {"error" : repr(e)}
 
 		self.response.headers["Content-Type"] = "text/javascript"
-		self.response.out.write("%s(%s);" % (jsonp, simplejson.dumps(response)))
-	
+		self.response.out.write("%s(%s);" % (jsonp, json.dumps(response)))
+
 	def print_home(self):
 		# Print a home page
 		self.response.headers["Content-Type"] = "text/html"
--- mirror.py
+++ mirror.py
@@ -17,12 +17,12 @@ MAX_FILE_SIZE = 1000000
 
 def get(url):
 	'''Get a file, using memcache if possible'''
-	
+
 	# Check memcache for this url
 	data = memcache.get(url)
 	if data is not None:
 		return data
-	
+
 	# Missed! Attempt to download it now
 	else:
 		try:
@@ -31,22 +31,22 @@ def get(url):
 			raise ProxyError('could not contact server, or server too slow', e)
 		except urlfetch.ResponseTooLargeError, e:
 			raise ProxyError('file is too big', e)
-		
+
 		data = result.content
-		
+
 		# HTTP Status code
 		if result.status_code != 200:
 			raise ProxyError('got status code %d for url %s' % (result.status_code, url))
 
 		# Check it's a valid glulx/zcode story file, before downloading the rest
-		if data.startswith('FORM') or data.startswith('Glul') or ord(data[0]) < 9:
-			pass
-		else:
-			raise ProxyError('url does not contain a story file')
-		
+		#if data.startswith('FORM') or data.startswith('Glul') or ord(data[0]) < 9:
+		#	pass
+		#else:
+		#	raise ProxyError('url does not contain a story file')
+
 		# All good... cache it and return (don't cache big files for now)
 		if len(data) <= MAX_FILE_SIZE:
 			if not memcache.add(url, data, 3600):
 				logging.error('Memcache set failed for url ' + url)
-		
+
 		return data
